c21c07f6b936d8cf43e5a5e47a8dc3844fbc51ed
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-11 16:38:40 +0530

    Add files via upload

---
+++ LoopTest/LoopTest/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LoopTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("Printing " + name + " 5 times");
+            Console.WriteLine("Using For Loop: ");
+            for(int i = 0; i < num; i++)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("Using While Loop: ");
+            while(num != 0)
+            {
+                Console.WriteLine(name);
+                num--;
+            }
+        }
+    }
+}
